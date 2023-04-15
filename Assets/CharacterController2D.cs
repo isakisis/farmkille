f26827a84081052f1c706c54a627b606a472afbb
Casey Gu <casey.gu@visier.com>
2023-04-15 15:03:43 -0700

    comrade_1 movement script

--- Assets/CharacterController2D.cs
+++ Assets/CharacterController2D.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class CharacterController2D : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Rigidbody2D rigidbody2d;
+    [SerializeField] float speed = 2f;
+    Vector2 motionVector;
+
+    void Awake()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();    
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        motionVector = new Vector2(
+            Input.GetAxisRaw("Horizontal"), 
+            Input.GetAxisRaw("Vertical")
+        );
+    }
+
+    void FixedUpdate()
+    {
+        Move();        
+    }
+
+    private void Move()
+    {
+        rigidbody2d.velocity = motionVector * speed;
     }
 }
