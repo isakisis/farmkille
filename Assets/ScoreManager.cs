32399225efc3b5e5ebb6daf9fd457f1775a9c5eb
Chris Flanagan <crflanag@gmail.com>
2023-04-15 21:51:56 -0700

    Add timer and game over screen

--- Assets/ScoreManager.cs
+++ Assets/ScoreManager.cs
@@ -2,11 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ScoreManager : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI timerText;
+
+    public GameObject gameOverPanel;
+
     private int playerScore = 0;
+    private float timeRemaining = 60;
+
+    private bool gameOver = false;
 
     void Start()
     {
@@ -15,7 +23,18 @@ public class ScoreManager : MonoBehaviour
 
     void Update()
     {
-        
+        if (!gameOver) {
+            timeRemaining -= Time.deltaTime;
+            UpdateTimerText();
+
+            if (timeRemaining <= 0)
+            {
+                Time.timeScale = 0;
+
+                gameOverPanel.SetActive(true);
+                gameOver = true;
+            }
+        }
     }
 
     public void UpdateScore(int score) {
@@ -23,8 +42,28 @@ public class ScoreManager : MonoBehaviour
         UpdateScoreText();
     }
 
+    public void TryAgain()
+    {
+        Time.timeScale = 1;
+
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+    }
+
+    public void ReturnToMainMenu()
+    {
+        Time.timeScale = 1;
+
+        SceneManager.LoadScene(0);
+    }
+
     private void UpdateScoreText()
     {
         scoreText.text = "Score: " + playerScore;
+    }
+
+    private void UpdateTimerText()
+    {
+        timerText.text = "Time: " + ((int) timeRemaining);
     }
 }
