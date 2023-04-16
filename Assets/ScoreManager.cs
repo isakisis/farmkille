6484c7465eca9f84e61630d6b693e93d37f725f9
Chris Flanagan <crflanag@gmail.com>
2023-04-15 20:02:22 -0700

    Update barrels

--- Assets/ScoreManager.cs
+++ Assets/ScoreManager.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public TextMeshProUGUI scoreText;
+    private int playerScore = 0;
+
     void Start()
+    {
+        UpdateScoreText();
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateScore(int score) {
+        playerScore += score;
+        UpdateScoreText();
+    }
+
+    private void UpdateScoreText()
     {
-        
+        scoreText.text = "Score: " + playerScore;
     }
 }
