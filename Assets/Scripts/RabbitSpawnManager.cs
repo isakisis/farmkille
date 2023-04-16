1975134b9520f217dd7058ee71c49a0aad818d54
Casey Gu <casey.gu@visier.com>
2023-04-15 19:57:29 -0700

    RabbitSpawnManager simplified

--- Assets/Scripts/RabbitSpawnManager.cs
+++ Assets/Scripts/RabbitSpawnManager.cs
@@ -6,20 +6,21 @@ public class RabbitSpawnManager : MonoBehaviour
 {
     public GameObject enemyRabbit;
     float time = 0;
-    float startDelay = 4;
+    float spawnDelay = 4;
 
     void Start()
     {
-        
+        StartCoroutine(EnemySpawn());
     }
 
     IEnumerator EnemySpawn()
     {
-        Boolean spawnEnemies = true;
+        bool spawnEnemies = true;
         while(spawnEnemies)
         {
             Vector3 spawnVector = new Vector3(Random.Range(-10f, 10f), -6, 0);
             Instantiate(enemyRabbit, spawnVector, Quaternion.identity);
+            yield return new waitForSeconds(spawnDelay);
         }
     }
 }
